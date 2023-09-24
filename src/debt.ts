25e5c8f6294273f8830bc647cb5ba5539cbca464
caspted <teddiejohn.rajeev-22@cpu.edu.ph>
2023-09-24 12:39:49 +0800

    [Ted][Config] added changes in configs

--- src/debt.ts
+++ src/debt.ts
@@ -1,1 +1,12 @@
+import * as readLineSync from 'readline-sync'
+
+while(true) {
+  const input = readLineSync.question('Who owes you and how much?');
+
+  console.log('The input is', input);
+
+  if(input == 'walang utang') {
+    break;
+  }
+}
 
